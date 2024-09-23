35798614e7d73b1606825ac72509e57add1ceddc
Andy <garsiaandy920@gmail.com>
2024-09-23 17:43:14 +0300

    test

--- js/server.js
+++ js/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = 4000
+const time = Date.now();
 
 app.get('/home', (req, res) => {
-    res.status(200).json('Welcome, your app is working well');
+    res.status(200).json('Welcome, your app is working well, server started at ' + time);
 })
 
 
